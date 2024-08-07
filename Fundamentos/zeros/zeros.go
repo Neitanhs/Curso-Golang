466685086051d0bba07aeed0155b03a94aa512da
neitanhs <nathanhs002@gmail.com>
2024-08-07 15:25:58 -0300

    Notes and Examples on Fundamental Parts of Golang (zero type)

---
+++ Fundamentos/zeros/zeros.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b float64
+	var c bool
+	var d string
+	var e *int //Ponteiro inteiro (ainda vamos estudar)
+	fmt.Printf("%v %v %v %q %v", a, b, c, d, e)
+	//Esse print vai retornar os valores default de casa da tipo
+}
